1d7f15dde73b9319694f9ac53b0e206ef9ba0364
donganguo <donganguo@meituan.com>
2021-08-03 20:19:21 +0800

    添加一个RPC服务提供者样例

--- tiny-rpc-registry/src/main/java/com/dragonfly/tiny/rpc/registry/service/impl/ZooKeeperServiceRegistry.java
+++ tiny-rpc-registry/src/main/java/com/dragonfly/tiny/rpc/registry/service/impl/ZooKeeperServiceRegistry.java
@@ -23,8 +23,9 @@ public class ZooKeeperServiceRegistry implements ServiceRegistry {
     private final ZkClient zkClient;
 
     public ZooKeeperServiceRegistry(String zkAddress) {
+        log.debug("connecting zookeeper");
         zkClient = new ZkClient(zkAddress, zkConstant.ZK_SESSION_TIMEOUT, zkConstant.ZK_CONNECTION_TIMEOUT);
-        log.debug("connect zookeeper");
+        log.debug("connected");
     }
 
     public void register(String serviceName, String serviceAddress) {
@@ -45,6 +46,11 @@ public class ZooKeeperServiceRegistry implements ServiceRegistry {
             String addressNode = zkClient.createEphemeralSequential(addressPath, serviceAddress);
             log.debug("create address node: {}", addressNode);
         }
+    }
+
+    public static void main(String[] args) {
+        ZooKeeperServiceRegistry zooKeeperServiceRegistry = new ZooKeeperServiceRegistry("127.0.0.1:2181");
+        System.out.println(zooKeeperServiceRegistry);
     }
 
 }
